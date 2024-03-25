01d3cdfa693b09f681d58a2196b28525905d15e6
alexjam3sw <alexander2.woolley@live.uwe.ac.uk>
2024-03-25 18:49:49 +0000

    added audio interaction

---
+++ Assets/Scripts/Locker_open.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locker_open : MonoBehaviour
+{
+    public AudioClip DoorOpen;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            gameObject.GetComponent<AudioSource>().PlayOneShot(DoorOpen);
+        }
+    }
+}
